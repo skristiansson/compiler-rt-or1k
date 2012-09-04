098df34a5a6b1729187010d7c63a415426915af0
Stefan Kristiansson <stefan@oompa.chokladfabriken>
2012-09-04 15:50:29 +0300

    OR1K: differentiate between linux and elf toolchain

--- make/platform/or1k.mk
+++ make/platform/or1k.mk
@@ -1,9 +1,12 @@
 Description := OpenRISC 1000
 
-Configs := or1k
+Configs := elf linux
 Arch := or1k
 
 CC := clang
 
-CFLAGS := -Wall -Werror -O3 -D_YUGA_LITTLE_ENDIAN=0 -D_YUGA_BIG_ENDIAN=1 -ccc-host-triple or1k -ccc-gcc-name or1k-elf-gcc
+CFLAGS := -Wall -Werror -O3 -D_YUGA_LITTLE_ENDIAN=0 -D_YUGA_BIG_ENDIAN=1 
+CFLAGS.elf := -ccc-host-triple or1k -ccc-gcc-name or1k-elf-gcc
+CFLAGS.linux := -ccc-host-triple or1k-linux
+
 FUNCTIONS := $(CommonFunctions)
